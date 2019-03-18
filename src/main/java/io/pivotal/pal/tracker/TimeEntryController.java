44baa20edbaf36697cc608d5983ff3202886aee4
ash-win-i <ashwini.kumari@wipro.com>
2019-03-17 22:25:34 -0700

    commented import TIF

--- src/main/java/io/pivotal/pal/tracker/TimeEntryController.java
+++ src/main/java/io/pivotal/pal/tracker/TimeEntryController.java
@@ -4,7 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import javax.imageio.plugins.tiff.TIFFDirectory;
+//import javax.imageio.plugins.tiff.TIFFDirectory;
 import java.util.List;
 
 @RestController
